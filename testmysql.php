b79c37f9c17bf73e85578f2f8abaa4151e3d0ea4
Neroist <wbolin@kth.se>
2015-04-07 14:56:52 +0200

    Deleted trash & trying sql

--- testmysql.php
+++ testmysql.php
@@ -1,7 +1,18 @@
 <?php 
-$link = mysql_connect('hostname','dbuser','dbpassword'); 
+$link = mysqli_connect('localhost','root',''); 
 if (!$link) { 
-	die('Could not connect to MySQL: ' . mysql_error()); 
+	die('Could not connect to MySQL: ' . mysqli_error($link)); 
 } 
-echo 'Connection OK'; mysql_close($link); 
+echo 'Connection OK'; #mysqli_close($link);
+$sql = "CREATE TABLE secure_login.Users (
+	userId int,
+	email varchar(40) PRIMARY KEY,
+	password varchar(15));";
+
+if(mysqli_query($link, $sql)){
+	echo "tut";
+}else{
+	echo "macarena";
+}
+
 ?>
