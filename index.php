6146b19060c8d716ce593b56617eefc9a8e89aa6
Neroist <wbolin@kth.se>
2015-04-09 16:25:58 +0200

    Added login form

--- index.php
+++ index.php
@@ -1,14 +1,51 @@
 <!DOCTYPE HTML> 
 <html>
 <head>
+
 <title>d2d</title>
 </head>
-<body> 
+<body style="background-color:#abcdef"> 
+<?php
+$regemailErr=$regpassErr = "";
+$regemail=$regpassword="";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if ($ema=empty($_POST["regemail"])) {
+     $regemailErr = "Email is required";
+   } else {
+     $regemail = $_POST["regemail"];
+   }
+   
+   if ($pas=empty($_POST["regpassword"])) {
+     $regpassErr = "Password is required";
+   } else {
+     $regpassword = $_POST["regpassword"];
+   }
+}
+?>
+<h2>REGISTRERA HÄR OM DU ÄR EN ANVÄNDARE</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+   E-mail: <input type="email" name="regemail">
+   <span class="error">* <?php echo $regemailErr;?></span>
+   <br><br>
+   Password: <input type="password" name="regpassword"></input>
+   <span class="error">* <?php echo $regpassErr;?></span>
+   <br><br>
+   <input type="submit" name="submit" value="Submit"> 
+</form>
+<p>
+  <?php
+  echo "<h2>Your Input:</h2>";
+  echo $regemail;
+  echo "<br>";
+  echo $regpassword;
+  echo "<br>";
+  ?>
+</p>
 
 <?php
 $emailErr=$passErr = "";
 $email=$password="";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    if ($ema=empty($_POST["email"])) {
      $emailErr = "Email is required";
@@ -20,21 +57,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
      $passErr = "Password is required";
    } else {
      $password = $_POST["password"];
-
    }
-   if(!$ema and !$pas){
-    header("Location: userpage.php");
-   }
-
 }
-
-
-echo "tut";
 ?>
-
-<h2>LOGGA IN HÄR OM DU ÄR EN ANVÄNDARE</h2>
-<p><span class="error">* required field.</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+<p>
+  <h2>Redan registrerad? Titta här.</h2>
+  <form method="post" action="userpage.php"> 
    E-mail: <input type="email" name="email">
    <span class="error">* <?php echo $emailErr;?></span>
    <br><br>
@@ -42,21 +70,10 @@ echo "tut";
    <span class="error">* <?php echo $passErr;?></span>
    <br><br>
    <input type="submit" name="submit" value="Submit"> 
-</form>
-<p>
-<?php
-echo "<h2>Your Input:</h2>";
-echo $email;
-echo "<br>";
-echo $password;
-echo "<br>";
-?>
+
 </p>
-<p>
 	<h2>Titta här om du är en drajver</h2>
 	<a href="driverpage.php">Click</a>
 </p>
-
-
 </body>
 </html>
