148243e4477009351d585a8b92015adcdd4cf460
Neroist <wbolin@kth.se>
2015-04-09 02:20:13 +0200

    Code shells
    La tills kodskal för vissa av de sidor vi kommer vilja ha

---
+++ buyerpage.php
@@ -0,0 +1,19 @@
+<html>
+<title>d2d Buyers page</title>
+<body>
+
+<p>
+<h3>Status on items you are buying:</h3>
+
+"A bunch of sql stuff"
+</p>
+
+
+<div>
+	<p>
+		Back to user page?
+	</p>
+</div>
+
+</body>
+</html>
---
+++ driverpage.php
@@ -0,0 +1,19 @@
+<html>
+<title>d2d Driver page</title>
+<body>
+
+<p>
+<h3>Enter here:</h3>
+
+Form där man ger sitt id
+</p>
+
+
+<div>
+	<p>
+		Back to user page?
+	</p>
+</div>
+
+</body>
+</html>
