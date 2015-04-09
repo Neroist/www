c5ec3527a920e6ef6c1f3064a2a8ef081cc79c1d
Neroist <wbolin@kth.se>
2015-04-09 16:51:09 +0200

    Slight testing

--- userpage.php
+++ userpage.php
@@ -1,6 +1,22 @@
 <html>
 <title>d2d Users page</title>
 <body>
+<?php  
+
+$link = mysqli_connect("localhost", "root", "");
+$email=$_POST["email"];
+$pass =$_POST["password"];
+$check_login_query = "SELECT email, password FROM d2d.Users WHERE email=$email and password=$pass";
+$result = mysqli_query($link, $check_login_query);
+
+if($result){
+	echo "mariachio";
+}else{
+	echo "tutbandet";
+}
+
+?>
+
 
 <p>
 <h3>Are you a seller?</h3>
