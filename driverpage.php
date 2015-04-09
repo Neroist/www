63a787b527823527a1c02b082a8198cc6c01d77f
Sam Andren <samand@kth.se>
2015-04-09 15:53:50 +0200

    creating form
    error with  $driverErr

--- driverpage.php
+++ driverpage.php
@@ -5,6 +5,34 @@
 <p>
 <h3>Enter here:</h3>
 
+<?php//define variables...
+$driverErr = $driverID = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+    $driverErr = "DriverID is required";
+  } else {
+    $driverID = $_POST["name"];
+  
+}?>
+
+
+<!--<p><span class="error">* required field.</span></p>-->
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+   DriverID: <input type="text" name="driverID">
+<!--   <span class="error">* <?php echo $driverErr;?></span> -->
+   <br><br>
+
+   <input type="submit" name="submit" value="Submit"> 
+</form>
+<p>
+
+
+
+
+
+
 Form där man ger sitt id
 </p>
 
