515e59773d5c07b592caa37becc1743a133bdb92
Sam Andren <samand@kth.se>
2015-04-15 23:21:12 +0200

    in progress

--- createDriverID.php
+++ createDriverID.php
@@ -5,7 +5,9 @@
 <h2>Create new driver ID by clicking button below</h2>
 <?php
 $link = mysqli_connect("localhost","root","");
-$get_max_query ="SELECT max("
+$get_max_query ="SELECT max(driverID) from Drivers;";
+$slask = mysqli_query($link, $get_max_query);
+echo $slask;
 ?>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 	 <input type="submit" name="submit" value="Submit"> 
