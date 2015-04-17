17222c7b9dc5ad3278ee070e935f6a6a40700f0c
Sam Andren <samand@kth.se>
2015-04-17 15:04:02 +0200

    forminputstuff

--- createDriverID.php
+++ createDriverID.php
@@ -5,15 +5,40 @@
 <h2>Create new driver ID by clicking button below</h2>
 <?php
 $link = mysqli_connect("localhost","root","");
-$get_max_query ="SELECT max(driverID) from Drivers;";
+$get_max_query ="SELECT max(driverID) FROM d2d.Drivers;";
 $slask = mysqli_query($link, $get_max_query);
-echo $slask;
+$tut = mysqli_fetch_row($slask);
+
+
+$bankAccErr=$bankRoutErr=$bankAcc=$bankRout="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if ($ema=empty($_POST["bankAcc"])) {
+     $regemailErr = "Enter bank account";
+   } else {
+     $bankAcc = $_POST["bankAcc"];
+   }
+   
+   if ($pas=empty($_POST["bankRout"])) {
+     $bankRoutErr = "Enter bank routing number";
+   } else {
+     $bankRout = $_POST["bankRout"];
+   }
+}
 ?>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-	 <input type="submit" name="submit" value="Submit"> 
-	</form>
+<h2>REGISTRERA HÄR OM DU ÄR EN ANVÄNDARE</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+   <span class="error">* <?php echo $bankAccErr;?></span>
+   <br><br>
+   <span class="error">* <?php echo $bankRoutErr;?></span>
+   <br><br>
+   <input type="submit" name="submit" value="Submit"> 
+</form>
+<p>
+
 	<p>
-		jaha dåså
+		<?php echo $tut[0]+1?>
 	</p>
 		
 </body>
