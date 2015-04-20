f4c1b8e1c5419ceafbe50e19c7355623e96a1a34
Neroist <wbolin@kth.se>
2015-04-20 15:48:28 +0200

    nykontrakt

---
+++ insertseller.php
@@ -0,0 +1,22 @@
+<?php
+
+function insert_seller($link, $email, $bankrouting, $bankaccount, $address){
+	$insert_query="INSERT INTO d2d.Sellers SET email='$email', address='$address', bankaccount='$bankaccount', bankrouting='$bankrouting';";
+	$result= mysqli_query($link, $insert_query);
+	if($result){
+
+		return true;
+	}else{
+		$update_query=" UPDATE d2d.Sellers 
+						SET address='$address', bankaccount='$bankaccount', bankrouting='$bankrouting'
+						WHERE email='$email';";
+		$result2=mysqli_query($link, $update_query);
+		if($result2){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+?>
---
+++ newcontract2.php
@@ -0,0 +1,68 @@
+<html>
+<?php 
+include("header.php");
+include("insertbuyer.php");
+$formError= "";
+$addErr=$emailErr="";
+$bEmail=$bAddress="";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $address_empty=empty($_POST["bAddress"]);
+      $email_empty=empty($_POST["bEmail"]);
+      
+       
+      if ($address_empty) {
+        $addErr = "Pick-up address is required";
+      } 
+      else{
+        $bAddress = $_POST["bAddress"];
+      }
+
+      if($email_empty){
+        $emailErr = "Bank routing number is required";
+      } 
+      else{
+        $bEmail = $_POST["bEmail"];
+
+      }
+
+      if($bEmail and $bAddress){
+        
+          if(insert_buyer($link, $_SESSION["useremail"], $bankrouting, $banknumber, $address)){
+
+          	header("Location: newcontract2.php");
+        }else{
+        	$formError = "Something went wrong, try again.";
+        }
+      }
+
+    }
+
+?>
+<body>
+<h2>Enter buyer's info here:</h2>
+
+
+
+
+
+<p><span class="error">* required field.</span></p>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+	   Buyer's email: <input type="email" name="bEmail">
+	   <span class="error">* <?php echo $emailErr;?></span>
+	   <br><br>
+	   Buyer's address: <input type="text" name="bAddress"></input>
+	   <span class="error">* <?php echo $addErr;?></span>
+	   <br><br>
+
+	   <input type="submit" name="submit" value="Submit"> 
+
+	</form>
+	<p>
+
+<p>
+	<?php echo $formError ?>
+</p>
+
+
+</body>
+</html>
