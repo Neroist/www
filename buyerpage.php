a99aa24f9394434d26b5371adf3965684fdb6351
Neroist <wbolin@kth.se>
2015-04-27 15:16:41 +0200

    almost done

--- buyerpage.php
+++ buyerpage.php
@@ -10,13 +10,27 @@
 		<?php 
 		include("getpurchases.php");
 		include("payfor.php");
+		include("confirmdeliv.php");
+		include("addSatisfaction.php");
 		if($_SERVER["REQUEST_METHOD"]==="POST"){
 			$empty_paid = empty($_POST["paid"]);
 			if(!$empty_paid){
 				payfor($link, $_POST["paid"]);
+				$_POST["paid"]="";
 				header("Location: $_SERVER[PHP_SELF]");
 			}
+			$empty_confirmed = empty($_POST["confirm"]);
+			if(!$empty_confirmed and !$empty_satisfaction){
+				add_satisfaction($link, $_POST["satisfactionID"], $_POST["satisfaction"]);
+				confirm_deliv($link, $_POST["confirm"]);
+				$_POST["confirm"]="";
+				$_POST["satisfaction"]="";
+				header("Location: $_SERVER[PHP_SELF]");
+			}
+			$empty_satisfaction = empty($_POST["satisfaction"]);
+			
 		}
+		var_dump($_POST);
 		getpurchases($link, $_SESSION["useremail"])
 		?>
 	
